c4c026867cb3b678eb6f1adf923dcc97f6d62f01
bharathganeshna <74003859+bharathganeshna@users.noreply.github.com>
2024-04-12 22:57:28 +0530

    Update pipeline.py

--- pyannote/audio/core/pipeline.py
+++ pyannote/audio/core/pipeline.py
@@ -1,156 +1,66 @@
-# MIT License
-#
-# Copyright (c) 2021 CNRS
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+@classmethod
+def from_pretrained(
+    cls,
+    checkpoint_path: Union[Text, Path],
+    hparams_file: Union[Text, Path] = None,
+    cache_dir: Union[Path, Text] = CACHE_DIR,
+) -> "Pipeline":
+    """Load pretrained pipeline from a local path
 
-import os
-import warnings
-from collections import OrderedDict
-from collections.abc import Iterator
-from functools import partial
-from pathlib import Path
-from typing import Callable, Dict, List, Optional, Text, Union
+    Parameters
+    ----------
+    checkpoint_path : Path or str
+        Path to pipeline checkpoint, assumed to be a local file path.
+    hparams_file: Path or str, optional
+        Path to a hyperparameters file.
+    cache_dir: Path or str, optional
+        Path to model cache directory. Defaults to pyannote's CACHE_DIR when unset.
 
-import torch
-import torch.nn as nn
-import yaml
-from huggingface_hub import hf_hub_download
-from huggingface_hub.utils import RepositoryNotFoundError
-from pyannote.core.utils.helper import get_class_by_name
-from pyannote.database import FileFinder, ProtocolFile
-from pyannote.pipeline import Pipeline as _Pipeline
+    Returns
+    -------
+    Pipeline
+        An instance of the requested pipeline.
+    """
 
-from pyannote.audio import Audio, __version__
-from pyannote.audio.core.inference import BaseInference
-from pyannote.audio.core.io import AudioFile
-from pyannote.audio.core.model import CACHE_DIR, Model
-from pyannote.audio.utils.reproducibility import fix_reproducibility
-from pyannote.audio.utils.version import check_version
+    checkpoint_path = str(checkpoint_path)
 
-PIPELINE_PARAMS_NAME = "config.yaml"
+    # Make sure the checkpoint path is a file that exists to avoid errors
+    if not os.path.isfile(checkpoint_path):
+        raise FileNotFoundError(f"Checkpoint file not found: {checkpoint_path}")
 
+    # Use the local path directly as the configuration file
+    config_yml = checkpoint_path
 
-class Pipeline(_Pipeline):
-    @classmethod
-    def from_pretrained(
-        cls,
-        checkpoint_path: Union[Text, Path],
-        hparams_file: Union[Text, Path] = None,
-        cache_dir: Union[Path, Text] = CACHE_DIR,
-    ) -> "Pipeline":
-        """Load pretrained pipeline
+    with open(config_yml, "r") as fp:
+        config = yaml.load(fp, Loader=yaml.SafeLoader)
 
-        Parameters
-        ----------
-        checkpoint_path : Path or str
-            Path to pipeline checkpoint, or a remote URL,
-            or a pipeline identifier from the huggingface.co model hub.
-        hparams_file: Path or str, optional
-        use_auth_token : str, optional
-            When loading a private huggingface.co pipeline, set `use_auth_token`
-            to True or to a string containing your hugginface.co authentication
-            token that can be obtained by running `huggingface-cli login`
-        cache_dir: Path or str, optional
-            Path to model cache directory. Defauorch/pyannote" when unset.
-        """
-        if isinstance(checkpoint_path, Path):
-            checkpoint_path = str(checkpoint_path)
-        if not os.path.isfile(checkpoint_path):
-            raise FileNotFoundError(f"Checkpoint file not found: {checkpoint_path}")
-        
-        
-            config_yml = checkpoint_path
+    if "version" in config:
+        check_version("pyannote.audio", config["version"], __version__, what="Pipeline")
 
-        else:
-            if "@" in checkpoint_path:
-                model_id = checkpoint_path.split("@")[0]
-                revision = checkpoint_path.split("@")[1]
-            else:
-                model_id = checkpoint_path
-                revision = None
+    # initialize pipeline
+    pipeline_name = config["pipeline"]["name"]
+    Klass = get_class_by_name(
+        pipeline_name, default_module_name="pyannote.pipeline.blocks"
+    )
+    params = config["pipeline"].get("params", {})
+    pipeline = Klass(**params)
 
-            try:
-                config_yml = hf_hub_download(
-                    model_id,
-                    PIPELINE_PARAMS_NAME,
-                    repo_type="model",
-                    revision=revision,
-                    library_name="pyannote",
-                    library_version=__version__,
-                    cache_dir=cache_dir,
-                    # force_download=False,
-                    # proxies=None,
-                    # etag_timeout=10,
-                    # resume_download=False,
-                    # use_auth_token=use_auth_token,
-                    # local_files_only=False,
-                    # legacy_cache_layout=False,
-                )
+    # freeze parameters
+    if "freeze" in config:
+        params = config["freeze"]
+        pipeline.freeze(params)
 
-            except RepositoryNotFoundError:
-                print(
-                    f"""
-Could not download '{model_id}' pipeline.
-It might be because the pipeline is private or gated so make
-sure to authenticate. Visit https://hf.co/settings/tokens to
-create your access token and retry with:
+    if "params" in config:
+        pipeline.instantiate(config["params"])
 
-   >>> Pipeline.from_pretrained('{model_id}'
-   ...                          )
+    if hparams_file is not None:
+        pipeline.load_params(hparams_file)
 
-If this still does not work, it might be because the pipeline is gated:
-visit https://hf.co/{model_id} to accept the user conditions."""
-                )
-                return None
-
-        with open(config_yml, "r") as fp:
-            config = yaml.load(fp, Loader=yaml.SafeLoader)
-
-        if "version" in config:
-            check_version(
-                "pyannote.audio", config["version"], __version__, what="Pipeline"
-            )
-
-        # initialize pipeline
-        pipeline_name = config["pipeline"]["name"]
-        Klass = get_class_by_name(
-            pipeline_name, default_module_name="pyannote.pipeline.blocks"
-        )
-        params = config["pipeline"].get("params", {})
-        pipeline = Klass(**params)
-
-        # freeze  parameters
-        if "freeze" in config:
-            params = config["freeze"]
-            pipeline.freeze(params)
-
-        if "params" in config:
-            pipeline.instantiate(config["params"])
-
-        if hparams_file is not None:
-            pipeline.load_params(hparams_file)
-
-        if "preprocessors" in config:
-            preprocessors = {}
-            for key, preprocessor in config.get("preprocessors", {}).items():
-                # preprocessors:
+    if "preprocessors" in config:
+        preprocessors = {}
+        for key, preprocessor in config.get("preprocessors", {}).items():
+	
+              # preprocessors:
                 #    key:
                 #       name: package.module.ClassName
                 #       params:
@@ -175,19 +85,20 @@ visit https://hf.co/{model_id} to accept the user conditions."""
                     template = preprocessor
                     preprocessors[key] = template
 
-            pipeline.preprocessors = preprocessors
+        pipeline.preprocessors = preprocessors
 
-        # send pipeline to specified device
-        if "device" in config:
-            device = torch.device(config["device"])
-            try:
-                pipeline.to(device)
-            except RuntimeError as e:
-                print(e)
+    # send pipeline to specified device
+    if "device" in config:
+        device = torch.device(config["device"])
+        try:
+            pipeline.to(device)
+        except RuntimeError as e:
+            print(e)
 
-        return pipeline
+    return pipeline
 
-    def __init__(self):
+
+ def __init__(self):
         super().__init__()
         self._models: Dict[str, Model] = OrderedDict()
         self._inferences: Dict[str, BaseInference] = OrderedDict()
@@ -346,3 +257,6 @@ visit https://hf.co/{model_id} to accept the user conditions."""
         self.device = device
 
         return self
+
+
+
